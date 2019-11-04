8c3a5588c641d2a95227b8600a05bd0a24cf931f
Noopur <41565380+naman1303@users.noreply.github.com>
2019-11-04 12:44:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,12 @@
 
 "use strict";
 
-var chalk = require('chalk'),
-    clear = require('clear'),
-    figlet = require('figlet');
+var chalk = require("chalk"),
+    clear = require("clear"),
+    figlet = require("figlet");
 
 clear();
-console.log("\n");
-console.log(
-    chalk.white(figlet.textSync('Here is the time!', { horizontalLayout: 'full' })
-    )
-);
+console.log("\n"+chalk.white(figlet.textSync("Here is the time!", { horizontalLayout: "full" })));
 
-var currentTime = 'Current date and time : ' + new Date();
-
-console.log("\n");
-console.log(currentTime);
-console.log("\n");
+var currentTime = "Current date and time : " + new Date();
+console.log("\n"+currentTime+"\n");
